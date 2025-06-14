171cafb81bbe793222ff230ae7b7f4336a5db9c1
jeremy-eth <jemybtc.eth@gmail.com>
2025-06-14 13:21:00 +0800

    Add Railway deployment configuration

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import re
 import time
 import json
 import random
+import os
 from threading import Lock
 
 app = Flask(__name__)
@@ -325,4 +326,5 @@ If code would be helpful, include:
         return jsonify({"error": error_msg}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
